9e9b725b6d05e76aeda357e73639d0a6ad3e5045
nopiarahman <nopiarahman93@gmail.com>
2021-05-20 00:43:59 +0700

    model rab

---
+++ app/rab.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class rab extends Model
+{
+    protected $table = 'rab';
+    protected $guarded = ['id','created_at','updated_at'];
+}
