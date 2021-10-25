358c18a5d07c6d6995eacd07eb7fcac387db01ef
nopiarahman <nopiarahman93@gmail.com>
2021-10-25 14:07:49 +0700

    perbaiki alokasi gudang +20 detik

--- app/Http/Controllers/GudangController.php
+++ app/Http/Controllers/GudangController.php
@@ -138,8 +138,8 @@ class GudangController extends Controller
     }
     public function hapusAlokasi(alokasiGudang $id){
         // dd($id);
-        $dari = Carbon::parse($id->created_at)->subSeconds(5);
-        $sampai = Carbon::parse($id->created_at)->addSeconds(5);
+        $dari = Carbon::parse($id->created_at)->subSeconds(20);
+        $sampai = Carbon::parse($id->created_at)->addSeconds(20);
         $cekPettyCash = pettyCash::where('uraian',$id->uraian)->whereBetween('created_at',[$dari,$sampai])->where('debet',$id->debet)->first();
         // dd($cekPettyCash);
         if($cekPettyCash != null){
