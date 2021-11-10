fd454f758825679c84b53ba05594e8c650791e84
nopiarahman <nopiarahman93@gmail.com>
2021-11-10 15:48:27 +0700

    perbaiki tempo dp off update

--- resources/views/cetak/kwitansiDp.blade.php
+++ resources/views/cetak/kwitansiDp.blade.php
@@ -142,11 +142,11 @@
             <td>: 1-10 {{Carbon\Carbon::parse($tempo)->isoFormat('MMMM YYYY')}}
             </td>
           </tr>
-          <tr>
+          {{-- <tr>
             <th>Jatuh Tempo</th>
             <td>: 1-10 {{Carbon\Carbon::parse($id->tempo)->isoFormat('MMMM YYYY')}}
             </td>
-          </tr>
+          </tr> --}}
           @endif
         </table>
       </div>
