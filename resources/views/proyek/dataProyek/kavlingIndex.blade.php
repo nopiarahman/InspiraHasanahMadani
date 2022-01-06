8ca9183d92f0733100d973abcfe2c6b61dd3113e
nopiarahman <nopiarahman93@gmail.com>
2022-01-06 16:16:42 +0700

    d

--- resources/views/proyek/dataProyek/kavlingIndex.blade.php
+++ resources/views/proyek/dataProyek/kavlingIndex.blade.php
@@ -143,7 +143,7 @@
               @endif
             </td>
             @if($kavling->pembelian != null)
-            <td>d
+            <td>
               @if($kavling->pelanggan !=null)
               <a href="{{route('pelangganDetail',['id'=>$kavling->pelanggan->id])}}" class="text-primary">
               @endif
