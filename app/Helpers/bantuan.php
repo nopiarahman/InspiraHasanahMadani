91fc606467c7786c5f79946aca578b25f3541c75
nopiarahman <nopiarahman93@gmail.com>
2022-07-13 06:21:15 +0700

    update saldo home

--- app/Helpers/bantuan.php
+++ app/Helpers/bantuan.php
@@ -80,7 +80,7 @@ function jenisKepemilikan($id)
 }
 function saldoTerakhir()
 {
-    $transaksi = DB::table('transaksi')->select('kredit', 'debet')->where('proyek_id', proyekId())->where('tanggal','<',Carbon::now()->isoFormat('YYYY-MM-DD'));
+    $transaksi = DB::table('transaksi')->where('tambahan',0)->select('kredit', 'debet')->where('proyek_id', proyekId())->where('tanggal','<',Carbon::now()->isoFormat('YYYY-MM-DD'));
     // dd($transaksi->last());
     $saldo = $transaksi->sum('kredit') - $transaksi->sum('debet');
     return $saldo;
